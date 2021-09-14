0828a53a5bd553bffc93b5b2c812922e7e51bad6
Darlan H <darlanhermessjb@hotmail.com>
2021-09-13 21:52:22 -0300

    feat: categories create and list

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import categoriesRoutes from './routes/categories.routes';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.get('/', (req, res) => {
     res.json(new Date());
